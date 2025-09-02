8d807ac7028548343c43669b99b0aa0ef2f2cba6
Michael Bestas <mkbestas@lineageos.org>
2025-09-02 20:43:42 +0300

    Revert "techpack: display: Add HBM sysfs node"
    This reverts commit f889a43549590b2dc895bf8a394df8a8a996fad0.
    
    Change-Id: I341421d5e0a8a1edbee248607d8658e336248ead

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -360,7 +360,6 @@ struct dsi_panel {
 	bool fod_ui;
 	bool force_fod_ui;
 	bool force_fod_dim_alpha;
-	bool hbm_state;
 };
 
 bool dsi_display_all_displays_dead(void);
@@ -492,7 +491,6 @@ int dsi_panel_set_param(struct dsi_panel *panel,
 			struct msm_param_info *param_info);
 
 void dsi_panel_reset_param(struct dsi_panel *panel);
-void dsi_panel_set_custom_param(struct dsi_panel *panel);
 
 int dsi_panel_get_elvss_data(struct dsi_panel *panel);
 int dsi_panel_get_elvss_data_1(struct dsi_panel *panel);
